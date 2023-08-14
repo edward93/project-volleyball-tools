62d0ce2afce9413a808f62182c95c0d9c905b532
edward93 <edward.chopuryan@gmail.com>
2023-08-14 18:23:51 -0400

    adds initial timeline component

--- src/components/VolleyballTool/VolleyballTool.Component.tsx
+++ src/components/VolleyballTool/VolleyballTool.Component.tsx
@@ -2,6 +2,7 @@ import "../../styles/tools.scss";
 import PlayersComponent from "../Players/Players.Component";
 import InspectorComponent from "../Inspector/Inspector.Component";
 import ScoreboardComponent from "../Scoreboard/Scoreboard.Component";
+import TimelineComponent from "components/timeline/Timeline.Component";
 
 const Tools = () => {
   return (
@@ -9,6 +10,7 @@ const Tools = () => {
       <section className="vt-tools-main-area">
         <ScoreboardComponent />
         <PlayersComponent />
+        <TimelineComponent />
       </section>
       <section className="vt-tools-inspector">
         <InspectorComponent />
---
+++ src/components/timeline/Timeline.Component.tsx
@@ -0,0 +1,13 @@
+import { useAppSelector } from "reduxTools/hooks";
+import "styles/timeline.scss";
+
+
+const TimelineComponent = () => {
+  return (
+    <section className="vt-tools-timeline-section">
+      TODO: Add timeline element
+    </section>
+  )
+}
+
+export default TimelineComponent
