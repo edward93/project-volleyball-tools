757eaf5a56f2e7d303acc5d6fcef4844825329d6
edward93 <edward.chopuryan@gmail.com>
2024-03-04 23:34:28 -0500

    adds rotate button (UI only)

--- src/components/Toolbar/Toolbar.Component.tsx
+++ src/components/Toolbar/Toolbar.Component.tsx
@@ -1,11 +1,18 @@
 import ScoreboardComponent from "components/Scoreboard/Scoreboard.Component";
+import { faRotateRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button } from "@mantine/core";
 
 import styles from "./toolbar.module.scss";
 
 const ToolbarComponent = () => {
   return (
     <div className={styles.container}>
-      <section>Left toolbar</section>
+      <section className={styles.toolbarSection}>
+        <Button variant="outline" size="xs" radius="xs" color="gray" leftIcon={<FontAwesomeIcon icon={faRotateRight} />}>
+          Rotate
+        </Button>
+      </section>
       <section className={styles.scoreBoardContainer}>
         <ScoreboardComponent />
       </section>
